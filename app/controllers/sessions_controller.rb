36a47fce65e57795d97b7d41a0d61fd4d500c5da
Neil Seejoor <nvseejoor@gmail.com>
2018-08-16 23:31:21 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,25 @@
+
 class SessionsController < ApplicationController
   def new
   end
 
   def create
-    if !params[:name] || params[:name].empty?
-      redirect_to '/login'
-    else
-      session[:name] = params[:name]
-      redirect_to '/'
-    end
+    user = User.find_by(name: params[:user][:name])
+
+    user = user.try(:authenticate, params[:user][:password])
+
+    return redirect_to(controller: 'sessions', action: 'new') unless user
+
+    session[:user_id] = user.id
+
+    @user = user
+
+    redirect_to controller: 'welcome', action: 'home'
   end
 
   def destroy
-    session.delete :name
-  end
+    session.delete :user_id
 
+    redirect_to '/'
+  end
 end
